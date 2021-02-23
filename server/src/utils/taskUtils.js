73ed92f0fe7a4bda8ac5648c624362c8902022a5
samuelov1 <jeanlouisfromtoge@gmail.com>
2021-02-23 16:43:06 +0200

    Fix utils wrap every function that may throw with try/catch

--- server/src/utils/taskUtils.js
+++ server/src/utils/taskUtils.js
@@ -5,102 +5,132 @@ import NotFoundError from "../errors/NotFoundError";
 const collectionName = "tasks";
 
 export const findAllTasks = async () => {
-  const query = {
-    parentId: { $exists: false }
-  };
-  const parentTasks = await DB.find(collectionName, query);
-  return Promise.all(parentTasks.map(populateTask));
+  try {
+    const query = {
+      parentId: { $exists: false }
+    };
+    const parentTasks = await DB.find(collectionName, query);
+    return Promise.all(parentTasks.map(populateTask));
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const populateTask = async (task) => {
-  if (task.subtasks.length === 0) return task;
+  try {
+    if (task.subtasks.length === 0) return task;
 
-  const query = {
-    parentId: ObjectId(task._id)
-  };
-  const subtasks = await DB.find(collectionName, query);
-  const populatedSubtasks = await Promise.all(subtasks.map(populateTask));
-  const populatedTask = { ...task, subtasks: populatedSubtasks };
+    const query = {
+      parentId: ObjectId(task._id)
+    };
+    const subtasks = await DB.find(collectionName, query);
+    const populatedSubtasks = await Promise.all(subtasks.map(populateTask));
+    const populatedTask = { ...task, subtasks: populatedSubtasks };
 
-  return populatedTask;
+    return populatedTask;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const findById = async (id) => {
-  const task = await DB.findOne(collectionName, { _id: ObjectId(id) });
+  try {
+    const task = await DB.findOne(collectionName, { _id: ObjectId(id) });
 
-  if (!task) {
-    throw new NotFoundError(`Could not find task with the given ID: ${id}`);
+    if (!task) {
+      throw new NotFoundError(`Could not find task with the given ID: ${id}`);
+    }
+
+    const populatedTask = await populateTask(task);
+    return populatedTask;
+  } catch (err) {
+    throw err;
   }
-
-  const populatedTask = await populateTask(task);
-  return populatedTask;
 };
 
 export const setCompletedAndUpdateAncestor = async (id, isCompleted) => {
-  const updatedTask = await setCompletedRecursively(id, isCompleted);
-  if (!updatedTask.parentId) return updatedTask;
+  try {
+    const updatedTask = await setCompletedRecursively(id, isCompleted);
+    if (!updatedTask.parentId) return updatedTask;
 
-  const updatedAncestor = await updateParentTask(
-    updatedTask.parentId,
-    updatedTask
-  );
+    const updatedAncestor = await updateParentTask(
+      updatedTask.parentId,
+      updatedTask
+    );
 
-  return updatedAncestor;
+    return updatedAncestor;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const setTaskCompleted = async (id, isCompleted) => {
+  try {
+    const filter = { _id: ObjectId(id) };
+    const update = { $set: { isCompleted } };
+    const options = { returnOriginal: false };
+
+    const task = await DB.findOneAndUpdate(
+      collectionName,
+      filter,
+      update,
+      options
+    );
+    if (!task) throw new NotFoundError(`Could not find task with ID: ${id}`);
+
+    return task;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const setCompletedRecursively = async (id, isCompleted) => {
-  const filter = { _id: ObjectId(id) };
-  const update = { $set: { isCompleted } };
-  const options = { returnOriginal: false };
+  try {
+    const task = await setTaskCompleted(id, isCompleted);
 
-  const task = await DB.findOneAndUpdate(
-    collectionName,
-    filter,
-    update,
-    options
-  );
+    task.subtasks = await Promise.all(
+      task.subtasks.map((subtaskId) =>
+        setCompletedRecursively(subtaskId, isCompleted)
+      )
+    );
 
-  if (!task) throw new NotFoundError(`Could not find task with ID: ${id}`);
-
-  task.subtasks = await Promise.all(
-    task.subtasks.map((subtaskId) =>
-      setCompletedRecursively(subtaskId, isCompleted)
-    )
-  );
-
-  return task;
+    return task;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const updateParentTask = async (parentId, task) => {
-  const parentTask = await DB.findOne(collectionName, {
-    _id: ObjectId(parentId)
-  });
-  if (!parentTask) {
-    throw new NotFoundError(`Could not find task with ID: ${parentId}`);
+  try {
+    const parentTask = await DB.findOne(collectionName, {
+      _id: ObjectId(parentId)
+    });
+
+    if (!parentTask) {
+      throw new NotFoundError(`Could not find task with ID: ${parentId}`);
+    }
+
+    const query = { parentId: ObjectId(parentId) };
+    if (task) query._id = { $ne: task._id };
+
+    const subtasks = await DB.find(collectionName, query);
+
+    parentTask.subtasks = await Promise.all(subtasks.map(populateTask));
+    if (task) parentTask.subtasks.push(task);
+
+    const allSubtasksCompleted = parentTask.subtasks.every(
+      (subtask) => subtask.isCompleted
+    );
+
+    if (parentTask.isCompleted === allSubtasksCompleted) return task;
+
+    parentTask.isCompleted = allSubtasksCompleted;
+    await setTaskCompleted(parentTask._id, allSubtasksCompleted);
+
+    return parentTask.parentId
+      ? updateParentTask(parentTask.parentId, parentTask)
+      : parentTask;
+  } catch (err) {
+    throw err;
   }
-
-  const query = { parentId: ObjectId(parentId) };
-  if (task) query._id = { $ne: task._id };
-
-  const subtasks = await DB.find(collectionName, query);
-
-  parentTask.subtasks = await Promise.all(subtasks.map(populateTask));
-  if (task) parentTask.subtasks.push(task);
-
-  const allSubtasksCompleted = parentTask.subtasks.every(
-    (subtask) => subtask.isCompleted
-  );
-
-  if (parentTask.isCompleted === allSubtasksCompleted) return task;
-
-  parentTask.isCompleted = allSubtasksCompleted;
-  const filter = { _id: ObjectId(parentId) };
-  const update = { $set: { isCompleted: allSubtasksCompleted } };
-  const options = { returnOriginal: false };
-
-  await DB.findOneAndUpdate(collectionName, filter, update, options);
-
-  return parentTask.parentId
-    ? updateParentTask(parentTask.parentId, parentTask)
-    : parentTask;
 };
